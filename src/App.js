93f51c5a90a8176d4865deecb84f43e14546ab21
Hazem Krimi <krimihazem1@gmail.com>
2020-04-16 15:14:07 +0100

    removed apollo client

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
 import { Switch, Route } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
 import Nav from './components/Nav';
-import LandingPage from './pages/LandingPage';
-
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_LINK
-});
 
 const App = () => (
-  <ApolloProvider client={client}>
+  <>
     <Nav />
     <Switch>
       <Route path='/' exact>
-        <LandingPage />
+        Landing Page
       </Route>
       <Route path='/about' exact>
         About page
@@ -24,7 +17,7 @@ const App = () => (
         404 Not Found
       </Route>
     </Switch>
-  </ApolloProvider>
+  </>
 );
 
 export default App;
